90f008a584d6f69fbaed344037d7a06dfb8b2ace
TejuKhatri <biduthkhatri33@gmail.com>
2025-07-30 10:07:01 +0545

    Initial commit

---
+++ src/components/Navbar/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='header-contents'>
+        <h1>Welcome to Food Express</h1>
+        <p>Order your favorite food online and enjoy fast delivery</p>  
+        <button >View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
