47b9f5139301792dd4577e63fe828eb4d3b59958
TejuKhatri <biduthkhatri33@gmail.com>
2025-07-31 15:19:48 +0545

    Your commit message

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import './Home.css'
-import Header from '../../components/Navbar/Header/Header'
+import React, { useState } from 'react';
+import './Home.css';
+import Header from '../../components/Navbar/Header/Header';
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
+import FoodDisplay from '../../components/foodDisplay/foodDisplay'; // ✅ path should match your file
+
 const Home = () => {
+  const [category, setCategory] = useState('All');
+
   return (
     <div>
-      <Header/>
+      <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} /> {/* ✅ Capitalized usage */}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
